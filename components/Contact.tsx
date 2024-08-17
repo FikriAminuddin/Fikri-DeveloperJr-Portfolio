83cdec133544ea7a1271faddb1be66e87d60d1e6
FikriAminuddin <muhammad.fikri.aminuddin.fauzi@possimo.com>
2024-08-17 23:34:30 +0800

    new update

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,36 +4,33 @@ const Contact: React.FC<{}> = () => {
   return (
     <section
       id="contact"
-      className="flex flex-col items-center justify-center h-full relative overflow-hidden pt-[35px] min-h-screen py-10"
+      className="flex flex-col items-center justify-center h-full relative overflow-hidden pt-10 min-h-screen py-10"
       style={{ transform: "scale(0.9)" }}
     >
-      <div className="flex flex-col justify-around flex-wrap items-center max-w-[900px]">
-        <h1 className="text-white font-semibold text-6xl">CONTACT ME</h1>
-        <p className="tracking-[0.5em] text-transparent font-light pb-5 bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500 text-1xl">
+      <div className="flex flex-col justify-center items-center max-w-[90vw] px-4">
+        <h1 className="text-white font-semibold text-4xl md:text-6xl text-center mb-6">CONTACT ME</h1>
+        <p className="tracking-wide text-transparent font-light pb-3 bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500 text-base md:text-xl text-center">
           GET IN TOUCH
         </p>
-        <p className="text-gray-300 text-center">
+        <p className="text-gray-300 text-center leading-relaxed md:leading-normal">
           I&apos;m excited to connect and discuss potential opportunities. Whether
           you have a project in mind, want to collaborate, or just want to say
           hello, feel free to reach out!
-          <br />
-          <br />
-          You can reach me via email at{" "}
+        </p>
+        <div className="flex flex-wrap justify-center mt-6">
           <a
             href="mailto:mdfikriaminuddin@gmail.com"
-            className="text-blue-500 hover:underline"
+            className="text-blue-500 hover:underline mr-4 mb-2"
           >
-            my-email
-          </a>{" "}
-          or connect with me on{" "}
+            Email Me
+          </a>
           <a
             href="https://linkedin.com/in/muhd-fikri-aminuddin"
             className="text-blue-500 hover:underline"
           >
             LinkedIn
           </a>
-          .
-        </p>
+        </div>
       </div>
     </section>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,52 +1,154 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar: React.FC<{}> = () => {
-  return (
-    <div className="w-full h-[65px] bg-[#111] fixed backdrop-blur-sm z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a
-          title="Fikri logo"
-          href="/"
-          className="h-auto w-auto flex flex-row items-center"
-        >
-          <Image
-            src="/Logo.svg"
-            alt="Fikri Aminuddin Logo"
-            width={100}
-            height={100}
-            sizes="100vw"
-            className="w-full h-auto"
-          />
-        </a>
+  const [isOpen, setIsOpen] = useState(false);
 
-        <div className="flex flex-row gap-5 text-white">
-          <a href="#about" className="hover:bg-[#2E2E2E] rounded-xl py-2 px-5">
-            About
-          </a>
-          <a
-            href="#experience"
-            className="hover:bg-[#2E2E2E] rounded-xl py-2 px-5"
-          >
-            Experience
-          </a>
-          <a href="#projects" className="hover:bg-[#2E2E2E] rounded-xl py-2 px-5">
-            Projects
-          </a>
-          {/* <div
-            onClick={() => window.open("mailto:mdfikriaminuddin@gmail.com")}
-            className="bg-transparent cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl py-2 px-5"
-          >
-            Contact
-          </div> */}
-          <a href="#contact" className="hover:bg-[#2E2E2E] rounded-xl py-2 px-5">
-            Contact
-          </a>
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <nav className="bg-[#111] fixed w-full z-50 backdrop-blur-sm">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <a href="/" className="flex items-center">
+                <Image
+                  src="/Logo.svg"
+                  alt="Fikri Aminuddin Logo"
+                  width={100}
+                  height={100}
+                />
+              </a>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <a
+                  href="#about"
+                  className="text-white hover:bg-[#2E2E2E] px-3 py-2 rounded-xl text-sm font-medium"
+                  onClick={closeMenu} // Close menu when clicked
+                >
+                  About
+                </a>
+
+                <a
+                  href="#experience"
+                  className="text-white hover:bg-[#2E2E2E] px-3 py-2 rounded-xl text-sm font-medium"
+                  onClick={closeMenu} // Close menu when clicked
+                >
+                  Experience
+                </a>
+
+                <a
+                  href="#projects"
+                  className="text-white hover:bg-[#2E2E2E] px-3 py-2 rounded-xl text-sm font-medium"
+                  onClick={closeMenu} // Close menu when clicked
+                >
+                  Projects
+                </a>
+
+                <a
+                  href="#contact"
+                  className="text-white hover:bg-[#2E2E2E] px-3 py-2 rounded-xl text-sm font-medium"
+                  onClick={closeMenu} // Close menu when clicked
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-white inline-flex items-center justify-center p-2 rounded-md hover:bg-[#2E2E2E] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-[#111] focus:ring-white"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      {isOpen && (
+        <div className="md:hidden bg-[#111] absolute top-16 w-full left-0">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <a
+              href="#about"
+              className="text-white block px-3 py-2 rounded-md text-base font-medium hover:bg-[#2E2E2E]"
+              onClick={closeMenu} // Close menu when clicked
+            >
+              About
+            </a>
+
+            <a
+              href="#experience"
+              className="text-white block px-3 py-2 rounded-md text-base font-medium hover:bg-[#2E2E2E]"
+              onClick={closeMenu} // Close menu when clicked
+            >
+              Experience
+            </a>
+
+            <a
+              href="#projects"
+              className="text-white block px-3 py-2 rounded-md text-base font-medium hover:bg-[#2E2E2E]"
+              onClick={closeMenu} // Close menu when clicked
+            >
+              Projects
+            </a>
+
+            <a
+              href="#contact"
+              className="text-white block px-3 py-2 rounded-md text-base font-medium hover:bg-[#2E2E2E]"
+              onClick={closeMenu} // Close menu when clicked
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      )}
+    </nav>
   );
 };
 
